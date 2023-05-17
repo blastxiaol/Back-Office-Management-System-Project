a601ccb8367a7b18b3b059153e8d8e704c0669e7
blastxiaol <blastxiaol@gmail.com>
2023-05-18 00:14:31 +0800

    init

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ConfigProvider } from 'antd';
+
+import storageUtils from './utils/storageUtils';
+import memoryUtils from './utils/memoryUtils';
+// import 'antd/dist/reset.css'
+
+const user = storageUtils.getUser();
+memoryUtils.user = user;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+  {/* <React.StrictMode> */}
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: '#00b96b',
+        },
+      }}
+    >
+      <App />
+    </ConfigProvider>
+  {/* </React.StrictMode> */}
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
