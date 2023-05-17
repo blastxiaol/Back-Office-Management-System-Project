ac87b638dc7d9494e4ecf7d21792f4dccce40f15
blastxiaol <blastxiaol@gmail.com>
2023-05-18 00:12:30 +0800

    home

---
+++ frontend/src/pages/home/index.jsx
@@ -0,0 +1,13 @@
+import './index.css';
+
+function Home() {
+    return (
+        <div className="home">
+            <h1>
+                Welcome to the Back Office System!
+            </h1>
+        </div>
+    )
+}
+
+export default Home;
